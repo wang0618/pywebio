cff86c3bfa2002ddf48c8a9f304811f7fe5d1512
WangWeimin <wang0.618@qq.com>
2024-04-20 23:57:12 +0800

    upgrade sphinx dependents

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,9 @@ extensions = [
     'sphinx_toolbox.collapse',
 ]
 
+# https://github.com/sphinx-doc/sphinx/issues/6316
+toc_object_entries = False
+
 primary_domain = "py"
 default_role = "py:obj"
 # intersphinx_mapping = {"python": ("https://docs.python.org/3.6/", None)}
@@ -71,9 +74,12 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ['static']
 
 
+
 def setup(app):
     """Configure Sphinx"""
     app.add_css_file('pywebio.css')
+    # from docutils.parsers.rst.directives.admonitions import Note
+    # app.add_directive('collapse', Note)
 
 
 # -- Extension configuration -------------------------------------------------
